3353f59f90c94fd6d385d12aac53fe641c6dccf5
Brando <brando.lugo@gmail.com>
2021-01-10 18:45:31 -0600

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
