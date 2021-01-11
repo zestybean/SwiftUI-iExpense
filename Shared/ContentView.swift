4073c42fd9066e34dee5bf47757ce4b3c759da3c
Brando <brando.lugo@gmail.com>
2021-01-10 18:47:14 -0600

    Initial project setup

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Init")
             .padding()
+        Text("Init")
+            .padding()
+        Text("Init")
+            .padding()
+        
+        
     }
 }
 
